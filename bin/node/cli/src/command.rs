7fd18fabc710aedf56ab47d31b2c588e16533010
sman <sman2013@163.com>
2020-04-30 07:01:38 +0800

    for origin pow (can compile) latest

--- bin/node/cli/src/command.rs
+++ bin/node/cli/src/command.rs
@@ -54,7 +54,7 @@ impl SubstrateCli for Cli {
 	fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
 		Ok(match id {
 			"|" | "dev" => Box::new(chain_spec::development_config()),
-			"local" => Box::new(chain_spec::local_testnet_config()),
+			"" | "local" => Box::new(chain_spec::local_testnet_config()),
 			//"" | "fir" | "flaming-fir" => Box::new(chain_spec::flaming_fir_config()?),
 			"staging" => Box::new(chain_spec::staging_testnet_config()),
 			path => Box::new(chain_spec::ChainSpec::from_json_file(
