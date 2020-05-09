41387a31785147c6db2c68cf9eab88a14939136f
sman <sman2013@163.com>
2020-05-09 17:53:49 +0800

    for node

--- bin/yee/node/src/chain_spec.rs
+++ bin/yee/node/src/chain_spec.rs
@@ -1,5 +1,5 @@
 use sp_core::{Pair, Public, sr25519};
-use node_runtime::{
+use yee_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
 	SudoConfig, SystemConfig, WASM_BINARY, Signature
 };
